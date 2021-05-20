2c71d7dfb36ac8ae9fe7c316247eaab883b304a7
yein <ehowldpdls@gmail.com>
2021-05-20 14:24:09 +0900

    숫자야구

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component, useCallback} from 'react';
-//import NumBaseball from './baseball/NumberBaseBall';  //숫자야구 NumberBaseBall.jsx
+import NumBaseball from './baseball/NumberBaseBall';  //숫자야구
 
 import TodoTemplate from './todoComponents/TodoTemplate';  // TodoTemplate, TodoHead , TodoList -> todolist 스타일
 import TodoCreate from './todoComponents/TodoCreate';  // input, 입력버튼
@@ -7,10 +7,12 @@ import TodoHead from './todoComponents/TodoHead'; // 날짜, 할일 갯수
 import TodoList from './todoComponents/TodoList'; // 각 리스트에 보여지는 것을 넣기
 import { TodoProvider } from './todoComponents/TodoContext';
 
-//todolist 
+
 const App = () => {
- 
+
+  //todolist 
   return (
+  <>
     <TodoProvider>
     <TodoTemplate>
       <TodoHead />
@@ -18,31 +20,40 @@ const App = () => {
       <TodoCreate />
     </TodoTemplate>
   </TodoProvider>
+
+{/* NumberBaseBall*/}
+    {/* <div class="wrap" 
+    style={{
+      width:"400px",
+      height:"500px",
+      margin:"50px auto",
+      borderRadius:"20px",
+      background:"#afd2f1",
+      boxShadow: "0 0 8px 0 rgba(0, 0, 0, 0.2)"
+      }}>  
+      <div> 
+        <h1 
+        style={{
+          textAlign:"center",
+          fontSize:"24px",
+          fontWeight:"bold",
+          paddingTop:"30px",
+          color: "#fff",
+          textShadow: "0 2px 8px 0 rgba(0, 0, 0, 1)"
+          }}
+          >Number BaseBall</h1>
+        <div class="random" style={{textAlign:"center"}}>
+            <div class="question">
+                <p class="question_text"></p>
+            </div>
+            <NumBaseball/>  
+          </div>  
+        </div>
+    </div> */}
+  </> 
   );
 }
 export default App;
 
-
-//NumberBaseBall.jsx에 쓰임
-// class App extends Component { //state를 위해서 사용함..
-//   render() {
-//     return (
-       /*
-            <div class="wrap">  
-              <div> 
-               <h1>숫자야구</h1>
-               <div class="random">
-                   <div class="question">
-                       <p class="question_text"></p>
-                    </div>
-                    <NumBaseball/>  
-                  </div>  
-               </div>
-            </div> */
-//     )
-//   }
-// }
-// export default App;
-
-//prop = property (태그의 속성을 설정해주는 것) , ex) <Myname name="react"/>  
+//prop = property (태그의 속성을 설정해주는 것) 
 //react는 내가 전달한 props를 가져가는 일을 한다.
--- src/baseball/TryNum.js
+++ src/baseball/TryNum.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-
 // const Try = (props) => { // 자식컴포넌트의 디폴트 상태. 내부에서 사용할땐 props.tryInfo 로 접근한다.
 const Try = ({ tryInfo }) => { // props를 구조분해 한 상태. 내부에선 tryInfo 로 바로 접근한다.
     return (
-        <div>
-            <h3 style={{ float:'left', margin:'0 10px 0 -35px' }}>※ {tryInfo.putnum}</h3>
-            <h3>{tryInfo.result}</h3>
+        <div style={{
+            width: "100%",
+            height: "45px"
+        }}>
+            <h3 style={{ 
+                float:'left',
+                margin:'10px 10px 0 50px',
+                color:"#394169"
+                }}
+                > {tryInfo.putnum}</h3>
+            <h3 style={{ 
+                float:'left',
+                margin:'10px 10px 0 15px',
+                color:"#394169"
+                }} 
+            >{tryInfo.result}</h3>
          </div>
     )
 };
